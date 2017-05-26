fba209695174452b3da778be99fba7e169f47ef8
kfili <kosta.filiotis@gmail.com>
2017-05-25 23:20:01 -0400

    Update config/environment.js for deployment
    - changed 'const ENV' to 'var ENV'
    - added "apiHost: 'http://localhost:3000/'," to 'var ENV' definition

--- config/environment.js
+++ config/environment.js
@@ -2,11 +2,12 @@
 
 module.exports = function (environment) {
   'use strict';
-  const ENV = {
+  var ENV = {
     modulePrefix: 'bike-cammers-map',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
+    apiHost: 'http://localhost:3000',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
